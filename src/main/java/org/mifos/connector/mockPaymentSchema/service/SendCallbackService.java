3f0b29eaba7c37224a55faa1d4df479aafed3267
Arka Das <arka.das@fynarfin.io>
2023-08-02 22:49:44 +0530

    GOV-301: refactored service

--- src/main/java/org/mifos/connector/mockPaymentSchema/service/SendCallbackService.java
+++ src/main/java/org/mifos/connector/mockPaymentSchema/service/SendCallbackService.java
@@ -2,19 +2,17 @@ package org.mifos.connector.mockPaymentSchema.service;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SendCallbackService {
 
-    public int sendCallback(String body, String callbackURL){
-        Response response = RestAssured.given()
+    public void sendCallback(String body, String callbackURL){
+        RestAssured.given()
                 .baseUri(callbackURL)
                 .contentType(ContentType.JSON)
                 .body(body)
                 .when()
                 .post();
-        return response.statusCode();
     }
 }
