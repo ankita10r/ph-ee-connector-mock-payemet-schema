83a0f3a274c5ab81edde03104276b8c1cc9cbf95
Arka Das <arka.das@fynarfin.io>
2023-08-02 18:45:14 +0530

    GOV-301: working solution with updated changes

--- src/main/java/org/mifos/connector/mockPaymentSchema/service/BatchService.java
+++ src/main/java/org/mifos/connector/mockPaymentSchema/service/BatchService.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mifos.connector.mockPaymentSchema.schema.AuthorizationRequest;
 import org.mifos.connector.mockPaymentSchema.schema.AuthorizationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +16,24 @@ import java.math.BigDecimal;
 @Service
 public class BatchService {
 
+    @Value("${bulk-processor.contactpoint}")
+    private String bulkProcessorContactPoint;
+
+    @Value("${threshold.amount}")
+    private String thresholdAmount;
+
     @Autowired
     private SendCallbackService sendCallbackService;
 
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Async("asyncExecutor")
     public void getAuthorization(String batchId, String clientCorrelationId,
-                                                  AuthorizationRequest authRequest, String callbackUrl){
+                                                  AuthorizationRequest authRequest, String callbackPath){
         AuthorizationResponse response = new AuthorizationResponse();
+        String callbackUrl = bulkProcessorContactPoint + callbackPath;
 
-        if (authRequest.getAmount().compareTo(BigDecimal.valueOf(200)) >= 0) {
+        if (authRequest.getAmount().compareTo(BigDecimal.valueOf(Long.valueOf(thresholdAmount))) >= 0) {
             response.setStatus("N");
             response.setClientCorrelationId(clientCorrelationId);
             response.setReason("Error getting authorization for the request");
@@ -30,7 +42,6 @@ public class BatchService {
             response.setClientCorrelationId(clientCorrelationId);
             response.setStatus("Y");
         }
-
         try {
             sendCallbackService.sendCallback(new ObjectMapper().writeValueAsString(response), callbackUrl);
         } catch (JsonProcessingException e) {
--- src/main/java/org/mifos/connector/mockPaymentSchema/service/SendCallbackService.java
+++ src/main/java/org/mifos/connector/mockPaymentSchema/service/SendCallbackService.java
@@ -1,16 +1,20 @@
 package org.mifos.connector.mockPaymentSchema.service;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SendCallbackService {
 
-    public void sendCallback(String body, String callbackURL){
-        RestAssured.given()
+    public int sendCallback(String body, String callbackURL){
+        Response response = RestAssured.given()
                 .baseUri(callbackURL)
+                .contentType(ContentType.JSON)
                 .body(body)
                 .when()
                 .post();
+        return response.statusCode();
     }
 }
